ccbf4029527047613d2b2bbf8cd5fe5bf6a35605
Seweryn Romanek <seweryn.romanek@gmail.com>
2022-02-18 16:28:36 +0100

    adding new task

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,47 @@
 {
-const tasks = [
-    {
-        content:"Pobawić się z psem",
-        done: false,
-    },
-    {
-        content:"Wyprowadzić na spacer",
-        done: true,
-    },
-];
+    const tasks = [
+        {
+            content: "Pobawić się z psem",
+            done: false,
+        },
+        {
+            content: "Wyprowadzić na spacer",
+            done: true,
+        },
+    ];
 
-const render = () => {
-   let htmlString = "";
+    const render = () => {
+        let htmlString = "";
 
-   for (const task of tasks) {
-htmlString += `
-               <li>
+        for (const task of tasks) {
+            htmlString += `
+               <li class="list__element ${task.done ? "list__element--done" : ""}">
                 ${task.content}
                </li>
               `;
-    document.querySelector(".js-tasks").innerHTML = htmlString;
- };
-};
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        };
+    };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent
+        });
+        render()
+    };
 
- const init = () => {
-render();
- };
- init();
+    const init = () => {
+        render();
+    };
+    const onFromSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent)
+    };
+    const from = document.querySelector(".js-from");
+    from.addEventListener("submit", onFromSubmit);
+
+    init();
 }
