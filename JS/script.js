83425279722610cf58dfe8f2d066b78d841e8596
Seweryn Romanek <seweryn.romanek@gmail.com>
2022-02-18 16:51:57 +0100

    remove task

--- JS/script.js
+++ JS/script.js
@@ -9,25 +9,37 @@
             done: true,
         },
     ];
-
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-               <li class="list__element ${task.done ? "list__element--done" : ""}">
-                ${task.content}
-               </li>
-              `;
-            document.querySelector(".js-tasks").innerHTML = htmlString;
-        };
-    };
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent
         });
         render()
     };
+const removeTask = (index) =>{
+    tasks.splice(index, 1);
+    render()
+}
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+               <li class="list__element ${task.done ? "list__element--done" : ""}">
+               <button class="js-remove">usuń</button>
+                ${task.content}
+               </li>
+              `;
+        }
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        
+            const removeButton = document.querySelectorAll(".js-remove");
+            removeButton.forEach((removeButton, index) => {
+                removeButton.addEventListener("click", () => {
+                removeTask(index)
+                });
+            });
+    };
+    
 
     const init = () => {
         render();
